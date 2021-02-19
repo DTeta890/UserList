46bde9722fef6525552d24608c7cf5e5186f20b0
Deivid Teta <dteta890@gmail.com>
2021-02-19 10:45:37 +0100

    Check And Pouse Icon for Active Status and Trash Icon for Delete

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
 $(document).ready(function () {
+    // Active Status Switch
     $("#container").on("click", ".toggle", function () {
-        let text = $(this).text();
-        if (text == "YES") {
+        if ($(this).hasClass("bi-check-circle-fill")) {
             alert(`Item with Id:${$(this).attr("id")} Status changed to passive!`);
-            $(this).text("NO");
-        } else {
+            $(this).removeClass("bi-check-circle-fill");
+            $(this).addClass("bi-pause-circle").css("color", "orange");
+        } else if ($(this).hasClass("bi-pause-circle-fill")) {
             alert(`Item with Id:${$(this).attr("id")} Status changed to active!`);
-            $(this).text("YES");
+            $(this).removeClass("bi-pause-circle-fill");
+            $(this).addClass("bi-check-circle").css("color", "green");
         }
     });
+    $("#container").on("mouseover", ".toggle", function () {
+        if ($(this).hasClass("bi-check-circle")) {
+            $(this).removeClass("bi-check-circle");
+            $(this).addClass("bi-check-circle-fill");
+        } else if ($(this).hasClass("bi-pause-circle")) {
+            $(this).removeClass("bi-pause-circle");
+            $(this).addClass("bi-pause-circle-fill");
+        }
+    });
+    $("#container").on("mouseout", ".toggle", function () {
+        if ($(this).hasClass("bi-check-circle-fill")) {
+            $(this).removeClass("bi-check-circle-fill");
+            $(this).addClass("bi-check-circle");
+        } else if ($(this).hasClass("bi-pause-circle-fill")) {
+            $(this).removeClass("bi-pause-circle-fill");
+            $(this).addClass("bi-pause-circle");
+        }
+    });
+    // Action Delete
     $("#container").on("click", ".delete", function () {
         let selectTr = `#container>#${$(this).attr("id")}`;
         $(selectTr).fadeOut();
     });
+    $("#container").on("mouseover", ".delete", function () {
+        $(this).removeClass("bi-trash");
+        $(this).addClass("bi-trash-fill");
+    });
+    $("#container").on("mouseout", ".delete", function () {
+        $(this).removeClass("bi-trash-fill");
+        $(this).addClass("bi-trash");
+    });
+    // Button Load
     $("#btnLoad").click(function () {
         $("#container").empty();
         $.ajax({
@@ -23,7 +53,7 @@ $(document).ready(function () {
             success: function (object) {
                 $.each(object.data, function (i, item) {
                     $("#container").append(
-                        `<tr id=${item.id}><td>${item.id}</td><td>${item.first_name}</td><td>${item.last_name}</td><td>${item.email}</td><td class='toggle' id='${item.id}'>YES</td><td class='delete' id='${item.id}'>DELETE</td></tr>`
+                        `<tr id=${item.id}><td>${item.id}</td><td>${item.first_name}</td><td>${item.last_name}</td><td>${item.email}</td><td><i id="1" class="bi-check-circle toggle" style="font-size: 1rem; color: green"></i></td><td><i id='${item.id}' class='bi-trash delete' style='font-size: 1rem; color: rgb(235, 62, 62)'></i></td></tr>`
                     );
                 });
             },
